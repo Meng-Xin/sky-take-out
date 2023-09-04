a8887a95be976b7d41481c17d80c44645d54e8ab
Meng-Xin <li997266754@gmail.com>
2023-09-04 23:09:07 +0800

    feat: 套餐模块增添 分页查询接口

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,12 +1,16 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
-import com.sky.entity.Dish;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,5 +54,18 @@ public class SetmealServiceImpl implements SetmealService {
             setmealDishMapper.insertBatch(setmealDishs);
         }
 
+    }
+
+    /**
+     * 套餐页面的分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO){
+        // 使用Spring工具进行分页查询
+        PageHelper.startPage(setmealPageQueryDTO.getPage(),setmealPageQueryDTO.getPageSize());
+        // 调用dao接口
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+        return new PageResult(page.getTotal(),page.getResult());
     }
 }
